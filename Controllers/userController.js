4002e33f8ac3fa9e1106165091beed6393c1c831
GeethaRagu <geethumsp@gmail.com>
2024-06-26 22:54:35 +0530

    updated forgotpassword resetpassword

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,9 @@
 import User from "../Models/userModel.js";
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import bcryptjs from "bcryptjs";
+dotenv.config();
+
 export const viewstudents = async (req, res, next) => {
   try {
     const students = await User.find().populate("courses");
@@ -46,7 +50,7 @@ export const postquery=async(req,res,next)=>{
         pass: process.env.PASSKEY,
       },
     });
-
+  
     const mailOptions = {
       from: process.env.PASSMAIL,
       to: email,
@@ -63,6 +67,66 @@ export const postquery=async(req,res,next)=>{
 
     await transporter.sendMail(mailOptions);
     res.status(200).json({ message: "Query sent successfully" });
+  } catch (error) {
+    next(error)
+  }
+}
+
+
+export const forgotpassword = async (req, res,next) => {
+  try {
+    const { email } = req.body;
+    const userinfo = await User.findOne({ email });
+    if (!userinfo) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.PASSMAIL,
+        pass: process.env.PASSKEY,
+      },
+    });
+
+    userinfo.save();
+    const clientUrl = process.env.CLIENT_URL;
+    const mailOptions = {
+      from: process.env.PASSMAIL,
+      to: userinfo.email,
+      subject: "Password Reset",
+      html: `
+              <p>Dear ${userinfo.username}</p>
+              <p>We received a request to reset your password. 
+              <p>Please click the following link to reset your password:</p>
+              <a href="${clientUrl}/resetpassword/">Reset Password</a>
+              <p>If you did not make this request, please ignore this email.</p>
+              <p>Thank you,</p>
+              <p>Suss Out</p>
+            `,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Password reset email sent successfully" });
+  } catch (error) {
+    next(error)
+  }
+};
+
+export const resetpassword=async(req,res,next)=>{
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const resetuser = await User.findOne({email});
+    console.log(resetuser);
+    const hashnewPassword = await bcryptjs.hashSync(password, 10);
+    const results = await User.updateOne({email:email},{email,password:hashnewPassword});
+    //console.log(results);
+    if(results.matchedCount===0){
+      return res.status(404).json({message:"User not found"})
+   }
+   const rest = await User.find({email}).populate("courses");
+ 
+   res.status(200).json({ message: "Password updated succeessfully", rest});
   } catch (error) {
     next(error)
   }
--- Routers/userRoute.js
+++ Routers/userRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { postquery, studentdetails, viewstudents } from '../Controllers/userController.js';
+import { forgotpassword, postquery, resetpassword, studentdetails, viewstudents } from '../Controllers/userController.js';
+import { verifyToken } from '../Middleware/verifyToken.js';
 
 const router  = express.Router();
 
 router.get('/viewstudents',viewstudents);
 router.post('/studentdetails',studentdetails);
 router.post('/postquery',postquery);
+
+router.post('/forgotpassword',verifyToken,forgotpassword);
+router.post('/resetpassword',verifyToken,resetpassword);
 export default router;
